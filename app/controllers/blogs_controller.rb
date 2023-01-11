a7cce1480b7525b18c6e808ab7ef7e7e7e7e05bb
Aung Htet Lin <aunghtetlin@gicjp.com>
2023-01-11 13:18:39 +0630

    Implemented partials with local data for portfolio and nav bar

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,43 +2,35 @@ class BlogsController < ApplicationController
   before_action :set_blog, only: %i[ show edit update destroy toggle_status ]
   layout "blog"
 
-  # GET /blogs or /blogs.json
   def index
     @page_title = "My Portfolio Blog"
     @blogs = Blog.all
   end
 
-  # GET /blogs/1 or /blogs/1.json
   def show
     @page_title = @blog.title
     @seo_keywords = @blog.body
   end
 
-  # GET /blogs/new
   def new
     @blog = Blog.new
   end
 
-  # GET /blogs/1/edit
   def edit
   end
 
-  # POST /blogs or /blogs.json
   def create
     @blog = Blog.new(blog_params)
 
     respond_to do |format|
       if @blog.save
         format.html { redirect_to blog_url(@blog), notice: "Blog was successfully created." }
-        # format.json { render :show, status: :created, location: @blog }
       else
         format.html { render :new, status: :unprocessable_entity }
-        # format.json { render json: @blog.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /blogs/1 or /blogs/1.json
   def update
     respond_to do |format|
       if @blog.update(blog_params)
@@ -51,7 +43,6 @@ class BlogsController < ApplicationController
     end
   end
 
-  # DELETE /blogs/1 or /blogs/1.json
   def destroy
     @blog.destroy
 
@@ -71,12 +62,10 @@ class BlogsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_blog
       @blog = Blog.friendly.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def blog_params
       params.require(:blog).permit(:title, :body)
     end
